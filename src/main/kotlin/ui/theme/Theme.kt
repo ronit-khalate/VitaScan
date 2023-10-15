7b029ee146468201e819235816cea94b30bb9023
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-15 17:08:31 +0530

    initial commit

---
+++ src/main/kotlin/ui/theme/Theme.kt
@@ -0,0 +1,76 @@
+package com.example.compose
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import ui.theme.*
+
+
+public val LightColors = Colors(
+    primary = md_theme_light_primary,
+    primaryVariant = md_theme_light_primaryContainer,
+    secondary = md_theme_light_secondary,
+    secondaryVariant = md_theme_light_secondaryContainer,
+    background = md_theme_light_background,
+    surface = md_theme_light_surface,
+    error = md_theme_light_error,
+    onPrimary = md_theme_light_onPrimary,
+    onSecondary = md_theme_light_onSecondary,
+    onBackground = md_theme_light_onBackground,
+    onSurface = md_theme_light_onSurface,
+    onError = md_theme_light_onError,
+    isLight = true
+)
+
+
+public val DarkColors = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,)
+//    outlineVariant = md_theme_dark_outlineVariant,
+//    scrim = md_theme_dark_scrim,
+//)
+//
+//@Composable
+//fun AppTheme(
+//  useDarkTheme: Boolean = isSystemInDarkTheme(),
+//  content: @Composable() () -> Unit
+//) {
+//  val colors = if (!useDarkTheme) {
+//    LightColors
+//  } else {
+//    DarkColors
+//  }
+//
+//  MaterialTheme(
+//    colorScheme = colors,
+//    content = content
+//  )
+//}
