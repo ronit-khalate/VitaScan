e5bd45830cef3a01cf7062922a666fc55486cb48
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-17 23:13:35 +0530

    Login function now handles navigation

--- src/main/kotlin/presentation/login_page/LoginScreenViewModel.kt
+++ src/main/kotlin/presentation/login_page/LoginScreenViewModel.kt
@@ -3,12 +3,18 @@ package presentation.login_page
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.push
 import domain.viewmodel.Viewmodel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import navigation.Screen
 
 
-class LoginScreenViewModel():Viewmodel {
+class LoginScreenViewModel(
+    val navigation:StackNavigation<Screen>
+):Viewmodel {
 
     var loginId by mutableStateOf("")
     var loginPassword by mutableStateOf("")
@@ -20,10 +26,11 @@ class LoginScreenViewModel():Viewmodel {
 
         showLoadingSpinner=true
 
-        viewmodelScope.launch {
-            delay(3000)
-            showLoadingSpinner=false
-        }
+        navigation.push(Screen.AdminHome)
+
+        showLoadingSpinner=false
+
+
 
     }
 
