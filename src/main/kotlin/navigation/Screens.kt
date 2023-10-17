3b308d6d890ee1f925c52dff2404d72b1ea7bea2
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-17 23:07:48 +0530

    Added All Navigation Components
    1. Screen class now extends Parcelable
    2.added LocalComponentContex
    3.added ProvideComponetContext function
    4. added ChildStack function

--- src/main/kotlin/navigation/Screens.kt
+++ src/main/kotlin/navigation/Screens.kt
@@ -1,7 +1,58 @@
 package navigation
 
-sealed class Screen(val route:String) {
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.StackAnimation
+import com.arkivanov.decompose.router.stack.StackNavigationSource
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.essenty.parcelable.Parcelable
+import com.arkivanov.essenty.parcelable.Parcelize
 
+
+sealed class Screen(val route:String):Parcelable{
+
+    @Parcelize
     object Login:Screen(route = "login_screen")
+    @Parcelize
     object AdminHome:Screen(route = "adminHome_screen")
+}
+
+val LocalComponentContext:ProvidableCompositionLocal<ComponentContext> =
+        staticCompositionLocalOf{ error("Root component contex was not provided") }
+
+@Composable
+fun ProvideComponentContext(componentContext: ComponentContext,content :@Composable ()-> Unit){
+
+    CompositionLocalProvider(LocalComponentContext provides componentContext, content=content)
+}
+
+@Composable
+inline fun <reified C : Parcelable> ChildStack(
+    source: StackNavigationSource<C>,
+    noinline initialStack: () -> List<C>,
+    modifier: Modifier = Modifier,
+    handleBackButton: Boolean = false,
+    animation: StackAnimation<C, ComponentContext>? = null,
+    noinline content: @Composable (C) -> Unit,
+) {
+    val componentContext = LocalComponentContext.current
+
+    Children(
+        stack = remember {
+            componentContext.childStack(
+                source = source,
+                initialStack = initialStack,
+                handleBackButton = handleBackButton,
+                childFactory = { _, childComponentContext -> childComponentContext },
+            )
+        },
+        modifier = modifier,
+        animation = animation,
+    ) { child ->
+        ProvideComponentContext(child.instance) {
+            content(child.configuration)
+        }
+    }
 }
