48d7b7c8a42bc148c35e5707122ab4b0dfa81636
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-17 23:14:42 +0530

    Main function how has components for navigation and some other changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,84 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.LocalScrollbarStyle
+import androidx.compose.foundation.defaultScrollbarStyle
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.jetbrains.lifecycle.LifecycleController
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.scale
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.push
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.example.compose.LightColors
+import navigation.ChildStack
+import navigation.ProvideComponentContext
+import navigation.Screen
+import presentation.admin_home_page.AdminHomePage
 import presentation.login_page.LoginScreen
 import presentation.login_page.LoginScreenViewModel
 
-@Composable
-@Preview
-fun App() {
-    val loginScreenViewModel by remember { mutableStateOf(LoginScreenViewModel()) }
 
-    MaterialTheme(
-        colors = LightColors
-    ) {
-       LoginScreen(modifier = Modifier.fillMaxSize(), viewModel = loginScreenViewModel)
+@OptIn(ExperimentalDecomposeApi::class)
+fun main() {
+
+    val lifecycle = LifecycleRegistry()
+    val rootComponentContext = DefaultComponentContext(lifecycle=lifecycle)
+    application {
+
+       val windowState = rememberWindowState()
+        LifecycleController(lifecycle,windowState)
+        Window(onCloseRequest = ::exitApplication) {
+
+
+
+            MaterialTheme(
+                colors = LightColors
+            ) {
+
+                CompositionLocalProvider(LocalScrollbarStyle provides defaultScrollbarStyle()){
+
+                    ProvideComponentContext(rootComponentContext){
+
+                        MainContent()
+                    }
+                }
+
+            }
+        }
+
+
+
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+@Composable
+fun MainContent(
+){
+
+    val navigation = remember { StackNavigation<Screen>() }
+    val loginScreenViewModel by remember { mutableStateOf(LoginScreenViewModel(navigation = navigation)) }
+
+    ChildStack(
+        source = navigation,
+        initialStack = { listOf(Screen.Login) },
+        handleBackButton = true,
+        animation = stackAnimation(fade())
+    ){screen ->
+
+        when(screen){
+            is Screen.AdminHome -> AdminHomePage()
+            is Screen.Login -> LoginScreen(modifier = Modifier.fillMaxSize(), viewModel = loginScreenViewModel)
+        }
+
     }
 }
