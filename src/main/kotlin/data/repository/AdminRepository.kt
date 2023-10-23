e8c7c7812d03b2878bfa55c36190b3c320321f08
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-23 10:49:00 +0530

    AdminRepository class renamed to AdminRepositoryImpl

--- src/main/kotlin/data/repository/AdminRepository.kt
+++ src/main/kotlin/data/repository/AdminRepository.kt
@@ -8,25 +8,9 @@ import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
-class AdminRepository :AdminRepository{
-    override suspend fun insertStaff(staff: Staff) {
-
-        val staffCount = transaction {
-            val staff= staff.userid.dropLast(4)
-            val count:Int=0
-
-            if(staff=="DOC"){
-
-                StaffCount.select {
-                    StaffCount.id eq 1
-                }
-            }
-        }
-        transaction {
-            StaffTable.insert {
-
-            }
-        }
+class AdminRepositoryImpl :AdminRepository{
+    override suspend fun addDoctor(staff: Staff) {
+        TODO("Not yet implemented")
     }
 
 
