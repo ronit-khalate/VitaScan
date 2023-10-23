fa3bec31750fb967f39034258e8ea5a5eb065999
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-23 13:57:37 +0530

    added loadDoc and updateDoc functions

--- src/main/kotlin/domain/repository/AdminRepository.kt
+++ src/main/kotlin/domain/repository/AdminRepository.kt
@@ -5,4 +5,7 @@ import domain.model.staff.Staff
 interface AdminRepository {
 
     suspend fun addDoctor(staff: Staff)
+    suspend fun loadDoc(staffId:String):Staff?
+
+    suspend fun updateDoc(staff: Staff)
 }
