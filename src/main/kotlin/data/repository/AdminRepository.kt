cfc4054fea2fbfc7ba2c76afa8c708df5eda9262
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-23 11:13:19 +0530

    addDoctor function implmented

--- src/main/kotlin/data/repository/AdminRepository.kt
+++ src/main/kotlin/data/repository/AdminRepository.kt
@@ -3,14 +3,41 @@ package data.repository
 import domain.model.staff.Staff
 import domain.model.staffcount.StaffCount
 import domain.model.staff.StaffTable
+import domain.model.staffcount.toStaffCountCls
 import domain.repository.AdminRepository
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 
 class AdminRepositoryImpl :AdminRepository{
     override suspend fun addDoctor(staff: Staff) {
-        TODO("Not yet implemented")
+
+
+        transaction {
+
+            val staffCountRow=StaffCount.select {
+                StaffCount.id eq 0
+            }.map { toStaffCountCls(it) }.firstOrNull()
+
+            staffCountRow?.let {staffCountCls ->
+
+                StaffTable.insert {
+                    it[userId]="DOC${staffCountCls.doctorCount+1}"
+                    it[firstName]=staff.firstName
+                    it[lastName]=staff.lastName
+                    it[mobile]=staff.mobile
+                    it[password]=staff.passWord
+                }
+
+                StaffCount.update(where = {StaffCount.id eq 0}){
+
+                    it[DoctorCount]=staffCountCls.doctorCount+1
+                }
+            }
+
+
+        }
     }
 
 
