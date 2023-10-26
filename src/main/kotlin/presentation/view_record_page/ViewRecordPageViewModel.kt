329fb3edacc91915e0e9dd9898d73d7f38e966fc
ronit-khalate <ronitkhalate243@gmail.com>
2023-10-26 14:47:04 +0530

    Created viewmodel for View record page

--- src/main/kotlin/presentation/view_record_page/ViewRecordPageViewModel.kt
+++ src/main/kotlin/presentation/view_record_page/ViewRecordPageViewModel.kt
@@ -1,4 +1,24 @@
 package presentation.view_record_page
 
-class ViewRecordPageViewModel {
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.popWhile
+import domain.viewmodel.Viewmodel
+import navigation.Screen
+
+class ViewRecordPageViewModel(
+    private  val navigation: StackNavigation<Screen>
+):Viewmodel {
+
+
+    fun onBackBtnClick(){
+
+        navigation.pop()
+    }
+
+    fun onLogOut(){
+        navigation.popWhile {
+            it::class!=Screen.Login::class
+        }
+    }
 }
